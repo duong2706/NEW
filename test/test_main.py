b0c6421dfb9811bb096bdf26860f6c55c68ef19a
duong2706 <nvduong887@gmail.com>
2024-05-15 21:32:13 +0700

    CICD

---
+++ test/test_main.py
@@ -0,0 +1,13 @@
+# Import các thư viện cần thiết
+from unittest.mock import patch
+import unittest
+
+# Hàm kiểm thử
+def test_example():
+    # Sử dụng các phương thức kiểm thử của Unity để kiểm tra điều kiện
+    assert 1 == 1
+
+# Kiểm tra main
+if __name__ == "__main__":
+    # Chạy kiểm thử
+    unittest.main()
